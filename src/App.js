b00f44c59d72a32fcb7ff4ab444331bac5c685ad
Dikshya Sharma <dikxu22@gmail.com>
2024-02-10 23:33:49 +0545

    updated

--- src/App.js
+++ src/App.js
@@ -111,7 +111,7 @@ function App(){
 
   return (
     <div className="App">
-      <Router basename="APIfetching">
+      <Router>
         <Routes>
           <Route path="/" element= { <Home /> }/>
 
